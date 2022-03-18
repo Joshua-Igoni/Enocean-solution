27c3a477ac7b2b383bc29966f4af8e7f200e6a8f
Josh bolten <jozhbolt@gmail.com>
2022-03-19 00:19:13 +0100

    added changes to script

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,6 @@
 
 sudo apt-get update
 sudo apt install -y docker.io nginx docker-compose
-wget https://www.openssl.org/source/old/1.1.0/openssl-1.1.0g.tar.gz
-tar xzvf openssl-1.1.0g.tar.gz
-cd openssl-1.1.0g
-./config
-make
-sudo make install
 openssl version -a
 cd ..
 docker-compose --version
