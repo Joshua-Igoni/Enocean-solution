b6c782cdbf893e4d894accab799e99177c652e80
Josh bolten <jozhbolt@gmail.com>
2022-03-19 00:13:34 +0100

    made changes to script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 
 sudo apt-get update
-sudo apt install -y docker.io nginx
+sudo apt install -y docker.io nginx docker-compose
 wget https://www.openssl.org/source/old/1.1.0/openssl-1.1.0g.tar.gz
 tar xzvf openssl-1.1.0g.tar.gz
 cd openssl-1.1.0g
@@ -11,15 +11,15 @@ make
 sudo make install
 openssl version -a
 cd ..
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-echo "password" > env_vars/a_password
-echo "username" > env_vars/a_username
-echo "34.221.56.204:8080" > env_vars/callback_url
-echo "password" > env_vars/g_password
-echo "username" > env_vars/g_username
-echo "IUBXY-NSFKR-QZPCI-HVMOQ" > env_vars/license_key
+cd env_vars
+echo "password" > a_password
+echo "username" > a_username
+echo "34.221.56.204:8080" > callback_url
+echo "password" > g_password
+echo "username" > g_username
+echo "IUBXY-NSFKR-QZPCI-HVMOQ" > license_key
+cd ..
 cd nginx
 openssl genrsa -out dev.localhost.key 2048
 openssl req -new -key dev.localhost.key -subj /CN=NG/ -out dev.localhost.crt
