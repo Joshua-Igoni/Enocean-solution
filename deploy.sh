e5bb80dfe2ed2ca2f66b468c6fff14f94881ab5b
Josh bolten <jozhbolt@gmail.com>
2022-03-19 01:01:17 +0100

    made changes

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,17 @@
 
 sudo apt-get update
 sudo apt install -y docker.io nginx docker-compose
+sudo usermod -aG docker $USER
+sudo chown $USER /var/run/docker.sock
 openssl version -a
 docker-compose --version
 cd deploy/env_vars
-echo "password" > a_password
-echo "username" > a_username
-echo "34.221.56.204:8080" > callback_url
-echo "password" > g_password
-echo "username" > g_username
-echo "IUBXY-NSFKR-QZPCI-HVMOQ" > license_key
+echo "password" > a_password.txt
+echo "username" > a_username.txt
+echo "34.221.56.204:8080" > callback_url.txt
+echo "password" > g_password.txt
+echo "username" > g_username.txt
+echo "IUBXY-NSFKR-QZPCI-HVMOQ" > license_key.txt
 cd ../nginx
 openssl genrsa -out dev.localhost.key 2048
 openssl req -new -key dev.localhost.key -subj /CN=NG/ -out dev.localhost.crt
