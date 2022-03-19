f0b5e57218cda12b241c8d48f935d93c459f972d
Josh bolten <jozhbolt@gmail.com>
2022-03-19 16:56:29 +0100

    modified shell script

--- deploy.sh
+++ deploy.sh
@@ -2,30 +2,15 @@
 
 
 sudo apt-get update
-sudo apt install -y docker.io nginx docker-compose
+sudo apt install -y docker.io docker-compose
 sudo usermod -aG docker $USER
 sudo chown $USER /var/run/docker.sock
 openssl version -a
 docker-compose --version
-export BASIC_AUTH_USERNAME=username
-export BASIC_AUTH_PASSWORD=password
-echo "52.43.164.208:8080" > callback_url.txt
-echo "password" > g_password.txt
-echo "username" > g_username.txt
-echo "IUBXY-NSFKR-QZPCI-HVMOQ" > license_key.txt
 cd deploy/nginx
 openssl genrsa -out dev.localhost.key 2048
 openssl req -new -key dev.localhost.key -subj /CN=NG/ -out dev.localhost.csr
-openssl x509 -req -days 3650 -in dev.localhost.csr -signkey dev.localhost.key -out dev.localhost.key
-mv dev.localhost.csr dev.localhost.crt 
-sudo mkdir -p /etc/nginx/certs
-sudo rm /etc/nginx/nginx.conf
-sudo mv ./nginx.conf /etc/nginx/
-sudo cp ./dev.localhost.crt /etc/nginx/certs 
-sudo cp ./dev.localhost.key /etc/nginx/certs
-sudo mv /etc/nginx/certs/dev.localhost.crt /etc/nginx/certs/certs.crt
-sudo mv /etc/nginx/certs/dev.localhost.key /etc/nginx/certs/certs.key 
-sudo systemctl restart nginx 
+openssl x509 -req -days 3650 -in dev.localhost.csr -signkey dev.localhost.key -out dev.localhost.key 
 cd ../local_deployment
 docker-compose up -d
 
