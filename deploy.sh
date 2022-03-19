50f0f2c947b7c204e71fe9bfe049e1dae793a99b
Josh bolten <jozhbolt@gmail.com>
2022-03-19 08:00:50 +0100

    made changes to shell

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,13 @@ cd ../nginx
 openssl genrsa -out dev.localhost.key 2048
 openssl req -new -key dev.localhost.key -subj /CN=NG/ -out dev.localhost.crt
 openssl x509 -req -days 3650 -in dev.localhost.crt -signkey dev.localhost.key -out dev.localhost.key
+sudo mkdir -p /etc/nginx/certs
 sudo rm /etc/nginx/nginx.conf
 sudo mv ./nginx.conf /etc/nginx/
+sudo cp ./dev.localhost.crt /etc/nginx/certs 
+sudo cp ./dev.localhost.key /etc/nginx/certs
+sudo mv /etc/nginx/certs/dev.localhost.crt /etc/nginx/certs/certs.crt
+sudo mv /etc/nginx/certs/dev.localhost.key /etc/nginx/certs/certs.key  
 cd ../local_deployment
 docker-compose up -d
 
