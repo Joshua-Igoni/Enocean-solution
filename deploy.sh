2e4d995d316be3b47c5e3ebe0f9fdeb9a9d9cccc
Josh bolten <jozhbolt@gmail.com>
2022-05-09 23:13:23 +0100

    made changes for ip

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ sudo chown $USER /var/run/docker.sock
 openssl version -a
 docker-compose --version
 cd deploy/nginx
-openssl req -newkey rsa:4096 -x509 -sha256 -nodes -days 365 -out dev.localhost.crt -keyout dev.localhost.key
+openssl req -newkey rsa:4096 -x509 -sha256 -nodes -days 365 -out dev.100.24.8.128.crt -keyout dev.100.24.8.128.key
 cd ../local_deployment
 docker-compose up
